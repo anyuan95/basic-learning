a92e63704616da9d49b573c7f6881381e67f38f9
anyuan95 <anyuan95@qq.com>
2021-08-23 22:04:47 +0800

    from PC：0823;

--- basic/online-judge/src/main/java/org/example/basic/oj/leetcode/Q46_need_better/Solution.java
+++ basic/online-judge/src/main/java/org/example/basic/oj/leetcode/Q46_need_better/Solution.java
@@ -1,4 +1,4 @@
-package org.example.basic.oj.leetcode.Q46_unfinished;
+package org.example.basic.oj.leetcode.Q46_need_better;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +8,8 @@ import java.util.stream.IntStream;
 /**
  * 全排列
  *
+ * FIXME 7 ms,击败了7.26% 的Java用户 ？ ？？ ？？？
+ *
  * @author anyuan
  * @since 2021-08-13 17:13
  */
@@ -15,18 +17,20 @@ class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        process(answer, nums, 0, 0);
+        process(answer, nums, 0);
         return answer;
     }
 
-    private void process(List<List<Integer>> answer, int[] nums, int currentIndex, int decided) {
-        if (decided == nums.length - 1) {
+    private void process(List<List<Integer>> answer, int[] nums, int currentIndex) {
+        // 这里本来currentIndex == nums.length 也可以的，但是减去1能少走一次递归。因为只剩一个值没处理了，处不处理都一样了。
+        if (currentIndex == nums.length - 1) {
             answer.add(IntStream.of(nums).boxed().collect(Collectors.toList()));
             return;
         }
         for (int i = currentIndex; i < nums.length; i++) {
             swap(nums, currentIndex, i);
-            process(answer, nums, i, decided + 1);
+            // 第一次写的时候这里搞错了，以为要遍历i。实际上需要遍历的是current+1，因为现在是已经确定好current位置的值了，该填current+1位置的值了。
+            process(answer, nums, currentIndex + 1);
             swap(nums, currentIndex, i);
         }
     }
