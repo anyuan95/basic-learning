e029febf17a86a4a89c826551e8549a1a48e707c
anyuan95 <anyuan95@qq.com>
2021-09-14 18:24:52 +0800

    Daily Commit: From T470P, Sept-14;

---
+++ basic/online-judge/src/main/java/org/example/basic/oj/leetcode/Q17/Solution.java
@@ -0,0 +1,27 @@
+package org.example.basic.oj.leetcode.Q17;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author anyuan
+ * @since 2021-09-14 18:19
+ */
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        int number = 0;
+        char[][] letters = new char[][]{
+                {'a','b','c'},
+                {'d','e','f'},
+                {'g','h','i'},
+                {'j','k','l'},
+                {'m','n','o'},
+                {'p','q','r','s'},
+                {'t','u','v'},
+                {'w','x','y','z'}
+        };
+
+        process()
+
+    }
+}
