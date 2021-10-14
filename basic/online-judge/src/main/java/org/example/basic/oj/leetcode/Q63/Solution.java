daefb4ab97ebf493b37833f9123c266da4ac0c75
anyuan <anyuan02@meituan.com>
2021-10-14 09:52:56 +0800

    from MAC: 1013; Q63

--- basic/online-judge/src/main/java/org/example/basic/oj/leetcode/Q63/Solution.java
+++ basic/online-judge/src/main/java/org/example/basic/oj/leetcode/Q63/Solution.java
@@ -10,18 +10,17 @@ class Solution {
         final int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
         dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
-        for (int i = 0; i < m; i++) {
-            if (obstacleGrid[i][0] == 1) {
-                dp[i][0] = 0;
-            }
-
+        // 先把第0行和第0列填上
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = obstacleGrid[0][j] == 1 ? 0 : dp[0][j - 1];
         }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
+                if (obstacleGrid[i][j] != 1) {
                     int ways = 0;
                     if (obstacleGrid[i-1][j] != 1) {
                         ways += dp[i-1][j];
@@ -34,5 +33,11 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        final Solution solution = new Solution();
+        System.out.println(solution.uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+        System.out.println(solution.uniquePathsWithObstacles(new int[][]{{0, 1}, {0, 0}}));
     }
 }
